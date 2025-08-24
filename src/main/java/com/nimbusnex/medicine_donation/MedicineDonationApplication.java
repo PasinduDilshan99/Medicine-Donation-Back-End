8b009e8cecad28b6d01d2d7062fce6b142354d62
Pasindu <pd.dimbulana@gmail.com>
2025-08-24 18:36:59 +0530

    initial commit

---
+++ src/main/java/com/nimbusnex/medicine_donation/MedicineDonationApplication.java
@@ -0,0 +1,13 @@
+package com.nimbusnex.medicine_donation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineDonationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineDonationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nimbusnex/medicine_donation/MedicineDonationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimbusnex.medicine_donation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineDonationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
